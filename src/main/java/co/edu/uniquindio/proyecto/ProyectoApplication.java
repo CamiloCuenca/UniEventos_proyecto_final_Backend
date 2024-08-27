3b52d0d81806f2237ab83de17509880da0240a88
Brandon Motentriste <sneid123@hotmail.com>
2024-08-27 14:37:47 -0500

    config - git

--- src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
+++ src/main/java/co/edu/uniquindio/proyecto/ProyectoApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoApplication {
-
+    /**
+     * prueba
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(ProyectoApplication.class, args);
     }
