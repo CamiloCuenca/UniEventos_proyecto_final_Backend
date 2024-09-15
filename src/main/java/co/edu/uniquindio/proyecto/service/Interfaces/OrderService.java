15f3eb9723ed50bc6abbe528b58a88b15e6d198f
Juan Camilo Cuenca Sepulveda <camilocuenca1810@gmail.com>
2024-09-15 18:16:05 -0500

    feat(order): create service methods in the corresponding interface for purchase orders: OrderService.java

--- src/main/java/co/edu/uniquindio/proyecto/service/Interfaces/OrderService.java
+++ src/main/java/co/edu/uniquindio/proyecto/service/Interfaces/OrderService.java
@@ -1,4 +1,26 @@
 package co.edu.uniquindio.proyecto.service.Interfaces;
 
+import co.edu.uniquindio.proyecto.model.PurchaseOrder.Order;
+
+import java.util.List;
+
 public interface OrderService {
+
+    // Crear una nueva orden
+    Order createOrder(Order order) throws Exception;
+
+    // Actualizar una orden existente
+    Order updateOrder(String orderId, Order order) throws Exception;
+
+    // Eliminar una orden
+    void deleteOrder(String orderId) throws Exception;
+
+    // Obtener una orden por su ID
+    Order getOrderById(String orderId) throws Exception;
+
+    // Listar todas las órdenes de un usuario específico
+    List<Order> getOrdersByUser(String userId) throws Exception;
+
+    // Listar todas las órdenes
+    List<Order> getAllOrders() throws Exception;
 }
